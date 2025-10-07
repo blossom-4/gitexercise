13707bf72734c30535a6b2c006a8f34ad5c2761f
BLOSSOM <ELIZA.HERBISON94@GMAIL.COM>
2025-10-07 18:45:54 +0800

    feat: bloss details

---
+++ src/data/template-user-bloss.js
@@ -0,0 +1,11 @@
+// this is a template to follow
+// for the data object.
+
+const data = {
+  name: "Bloss Herbison",
+  caption: "the best",
+  coverImageUrl: "https://www.google.com/imgres?q=bomb.com%20image&imgurl=http%3A%2F%2Fjayroeder.com%2Fuploads%2Fimages%2Fdrawings%2F2011%2F05%2FItsTheBomb.jpg&imgrefurl=http%3A%2F%2Fjayroeder.com%2Fdrawings%2F510-its-the-bomb-com&docid=FSwv4Wt3VXwFxM&tbnid=sIk_ear4S8hvwM&vet=12ahUKEwiR9aub85GQAxVZSfUHHef4AsAQM3oECBEQAA..i&w=650&h=683&hcb=2&ved=2ahUKEwiR9aub85GQAxVZSfUHHef4AsAQM3oECBEQAA",
+  bio: `But you can call me nighthawk.`,
+};
+
+export default data;
