4b98f7208a0144db3d489fe4efc9829bf6cabf19
Blossom Herbison <eliza.herbison94@gmail.com>
2025-10-07 19:16:49 +0800

    Revert "Adding my bio juliane"

--- src/data/template-user-bloss.js
+++ src/data/template-user-bloss.js
@@ -5,7 +5,7 @@ const data = {
   name: "Bloss Herbison",
   caption: "the best",
   coverImageUrl: "https://www.google.com/imgres?q=bomb.com%20image&imgurl=http%3A%2F%2Fjayroeder.com%2Fuploads%2Fimages%2Fdrawings%2F2011%2F05%2FItsTheBomb.jpg&imgrefurl=http%3A%2F%2Fjayroeder.com%2Fdrawings%2F510-its-the-bomb-com&docid=FSwv4Wt3VXwFxM&tbnid=sIk_ear4S8hvwM&vet=12ahUKEwiR9aub85GQAxVZSfUHHef4AsAQM3oECBEQAA..i&w=650&h=683&hcb=2&ved=2ahUKEwiR9aub85GQAxVZSfUHHef4AsAQM3oECBEQAA",
-  bio: `Call me nighthawk.`,
+  bio: `But you can call me nighthawk.`,
 };
 
 export default data;
